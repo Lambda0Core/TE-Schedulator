15ffb60e65fbeb357bd7dc879093c88c061f9f37
Bryce Huhtala <sketchius@gmail.com>
2023-04-10 14:54:06 -0400

    Create sidebar components (front-end)

---
+++ vue/src/components/SidebarComponentElement.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="element.link">{{ element.title }}</a>
+</template>
+
+<script>
+export default {
+    name: "sidebar-element",
+    props: ["element"]
+}
+</script>
+
+<style>
+
+</style>
