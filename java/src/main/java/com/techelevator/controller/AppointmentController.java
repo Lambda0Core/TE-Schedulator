eeae0fcd437d42264ec252cea88f2def2d3098bd
Bryce Huhtala <sketchius@gmail.com>
2023-04-10 16:42:55 -0400

    Create controllers for appointments, offices, reviews

---
+++ java/src/main/java/com/techelevator/controller/AppointmentController.java
@@ -0,0 +1,50 @@
+package com.techelevator.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@RestController
+@CrossOrigin
+public class AppointmentController {
+
+    /*private AppointmentDao appointmentDao;
+
+    public AppointmentController(AppointmentDao appointmentDao) {
+        this.appointmentDao = appointmentDao;
+    }
+
+    @GetMapping("/appointment/{id}")
+    public Appointment getAppointmentById(@RequestParam int id) {
+        // TODO Connect to DAO
+        return null;
+    }
+
+    @GetMapping("patient/appointment/{id}")
+    public Appointment[] getPatientAppointments(@RequestParam int id) {
+        // TODO Connect to DAO
+        return null;
+    }
+
+    @GetMapping("provider/appointment/{id}")
+    public Appointment[] getProviderAppointments(@RequestParam int id) {
+        // TODO Connect to DAO
+        return null;
+    }
+
+    @PostMapping("/appointment")
+    public void createAppointment() {
+        // TODO Connect to DAO
+    }
+
+    @PutMapping("/appointment/{id}")
+    public void updateAppointment(@RequestParam int id) {
+        // TODO Connect to DAO
+    }
+
+    @DeleteMapping("/appointment/{id}")
+    public void deleteAppointment(@RequestParam int id) {
+        // TODO Connect to DAO
+    }*/
+}
