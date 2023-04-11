db6bdb6f457571a7e8da57926db8deb93dec4fc6
Emily LeFebvre <Emily@Lefebvre.us>
2023-04-11 13:08:42 -0500

    added aptlist.vue

---
+++ vue/src/components/AptList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Apt-list">
+    <table>
+      <tbody>
+        <tr v-for="apt in this.$store.state.apt" v-bind:key="apt.id">
+          <td width="80%">
+            <router-link
+              v-bind:to="{ name: 'Home', params: { id: apt.id } }"
+            >{{ topic.title }}</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import AptService from "../services/AptService.js";
+
+export default {
+  name: "apt-list",
+  methods: {
+    get() {
+    AptService
+    this.getTopics();
+        }
+      }
+  }
+</script>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,56 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  <div class="layout">
+    <sidebar :sidebarOptions="options" />
   </div>
 </template>
 
 <script>
+
+
+import sidebar from "../components/SidebarComponent.vue";
+
+// import AptService from "../services/AptService";
+
 export default {
-  name: "home"
+  name: "PatientPage",
+
+  data() {
+    return {
+      options: [
+        {
+          title: "Upcoming Appointments",
+          icon: "calendar-regular.svg",
+          link: "/"
+        },
+        {
+          title: "Find a Provider (MAINTENANCE)",
+          icon: "briefcase-medical-solid.svg",
+          link: ""
+        },
+        {
+          title: "My Doctors (MAINTENANCE)",
+          icon: "user-doctor-solid.svg",
+          link: ""
+        }
+      ]
+    }
+  },
+  components: {
+    sidebar,
+  },
+
+  setup() {},
 };
 </script>
+
+
+
+
+<style>
+.layout {
+  display: grid;
+  grid-template-columns: minmax(20vw, 300px) 1fr;
+  height: 100%;
+  width: 100%;
+}
+</style>
