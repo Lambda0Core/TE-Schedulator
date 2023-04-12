fae9667cdb10922028662fb13905e19acc524b27
Bryce Huhtala <sketchius@gmail.com>
2023-04-12 16:46:21 -0400

    Restore ProviderLandingPage.vue

---
+++ vue/src/views/ProviderLandingPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout">
+    <sidebar :sidebarOptions="options" />
+    <div></div>
+  </div>
+</template>
+
+<script>
+import sidebar from "../components/SidebarComponent.vue";
+
+export default {
+  name: "ProviderPage",
+
+  data() {
+    return {
+      options: this.$store.state.providerSidebarOptions
+    }
+  },
+  components: {
+    sidebar,
+  },
+
+  setup() {},
+};
+</script>
+
+
+
+
+<style scoped>
+.layout {
+  display: grid;
+  grid-template-columns: fit-content 1fr;
+  height: 100%;
+  width: 100%;
+}
+</style>
