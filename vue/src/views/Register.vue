00be00080f953712e3ab1cb6e05166c831af0916
Max Shatskiy <shatskiymax@gmail.com>
2023-04-12 11:19:08 -0400

    user name

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -7,7 +7,13 @@
       </div>
       <div class="form-input-group">
         <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
+        <input
+          type="text"
+          id="username"
+          v-model="user.username"
+          required
+          autofocus
+        />
       </div>
       <div class="form-input-group">
         <label for="password">Password</label>
@@ -15,46 +21,84 @@
       </div>
       <div class="form-input-group">
         <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
+        <input
+          type="text"
+          id="confirmPassword"
+          v-model="user.confirmPassword"
+          required
+        />
+         </div>
+        <div class="form-input-group">
+        <label for="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          v-model="user.firstName"
+          required
+        />
+        </div>
+        <div class="form-input-group">
+        <label for="confirmPassword">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          v-model="user.lastName"
+          required
+        />
+      </div>
+      <div id="selectBox">
+        <label for="userType">User Type</label>
+        <select id="userType" v-model="user.role">
+          <option value="patient">Patient</option>
+          <option value="doctor">Doctor</option>
+          
+        </select>
       </div>
       <div id="registerButton">
-      <button type="submit">Create Account</button>
+        <button type="submit">Create Account</button>
       </div>
-      <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      
+      <p>
+        <router-link :to="{ name: 'login' }"
+          >Already have an account? Log in.</router-link
+        >
+      </p>
     </form>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
 
 export default {
-  name: 'register',
+  name: "register",
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        confirmPassword: "",
+        role: "patient",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -62,51 +106,51 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
 </script>
 
 <style scoped>
-h1{
+h1 {
   color: var(--primary800);
-  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 }
 .form-input-group {
   margin-bottom: 1rem;
 }
-input{
- justify-content: center; 
-   width: 200px;
+input {
+  justify-content: center;
+  width: 200px;
   height: 20px;
-  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
 }
 label {
   margin-right: 0.5rem;
   display: block;
   text-align: center;
   color: var(--primary800);
-  font: bold;  
+  font: bold;
 }
-form{
+form {
   height: fit-content;
- }
+}
 
-button{
+button {
   justify-content: center;
   width: 150px;
   height: 30px;
   border-radius: 6px;
   font-size: 16px;
-  background-color:  var(--primary400);
+  background-color: var(--primary400);
   color: var(--primary800);
   border: 3px solid var(--primary800);
   transition-duration: 0.4s;
@@ -116,16 +160,30 @@ button:hover {
   border: 3px solid var(--primary400);
   color: white;
 }
-#register{
+#register {
   display: flex;
-  justify-content: center;  
+  justify-content: center;
   height: 100vh;
 }
-#registerButton{
+#registerButton {
   display: flex;
   justify-content: center;
   width: 100%;
   align-items: center;
+}
+.selectBox{
+display: flex;  
+justify-content: center;
+}
+select{   
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  width: 150px;
+  height: 25px;
+  margin-bottom: 1rem;
+  
+
   
 }
 </style>
