69e925869621abeaf513ef6c323047d5eeda33f8
Andrew Jocke <andrewjocke@gmail.com>
2023-04-13 11:57:40 -0400

    save

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -83,7 +83,7 @@ export default {
         lastname: "",
         confirmPassword: "",
         role: "user",
-        titlename: "",
+        title: "",
         office: "",
       },
       offices: [],
