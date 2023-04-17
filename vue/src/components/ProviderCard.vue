5b33ac772b543595082fe28468dd563de7603b03
Max Shatskiy <shatskiymax@gmail.com>
2023-04-17 17:41:34 -0400

    review card styling

--- vue/src/components/ProviderCard.vue
+++ vue/src/components/ProviderCard.vue
@@ -1,31 +1,47 @@
 <template>
   <div class="container">
     <div class="identity">
-     
-      <h2 class = "fullname">Dr. {{ provider.firstName }} {{ provider.lastName }}</h2>
-      </div>
-      <h3>
-        {{ office.name }}
-         </h3>
-      <p> Located at: {{ office.address }} {{ office.cityName }}
-        {{ office.stateAcronym }}</p>
-      <p>Office Hours: {{ office.openTime }} to {{ office.closeTime }}</p>
-      <p class="phone">Phone Number: {{ office.phoneNumber }}</p>
+      <h2 class="fullname">
+        Dr. {{ provider.firstName }} {{ provider.lastName }}
+      </h2>
+    </div>
+    <h3>
+      {{ office.name }}
+    </h3>
+    <p>
+      Located at: {{ office.address }} {{ office.cityName }}
+      {{ office.stateAcronym }}
+    </p>
+    <p>Office Hours: {{ office.openTime }} to {{ office.closeTime }}</p>
+    <p class="phone">Phone Number: {{ office.phoneNumber }}</p>
 
-      
-    <div class ="button-container">
-    <div class="button">
-      <router-link
-        :to="{ name: 'leave-reviews', params: { providerId: provider.id, providerName: provider.firstName + ' ' + provider.lastName } }"
-        class="review-link">Leave a review</router-link
-      >
-    </div>
-    <div class="button">
-      <router-link
-        :to="{ name: 'book-appointment', params: { providerId: provider.id, providerName: provider.firstName + ' ' + provider.lastName } }"
-        class="appointment">Book Appointment</router-link
-      >
-    </div>
+    <div class="button-container">
+      <div class="button">
+        <router-link
+          :to="{
+            name: 'leave-reviews',
+            params: {
+              providerId: provider.id,
+              providerName: provider.firstName + ' ' + provider.lastName,
+            },
+          }"
+          class="review-link"
+          >Leave a review</router-link
+        >
+      </div>
+      <div class="button">
+        <router-link
+          :to="{
+            name: 'book-appointment',
+            params: {
+              providerId: provider.id,
+              providerName: provider.firstName + ' ' + provider.lastName,
+            },
+          }"
+          class="appointment"
+          >Book Appointment</router-link
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -72,16 +88,15 @@ export default {
   border-radius: 1rem;
   box-shadow: 15px 15px var(--primary200);
 }
-.identity{
+.identity {
   display: flex;
   align-items: center;
 }
 
 .fullname {
   flex: 1 1 auto;
-  
+
   grid-gap: 0.5rem;
-  
 }
 .img {
   display: inline-block;
@@ -113,7 +128,7 @@ h3 {
   color: var(--primary200);
   background-color: var(--primary400);
   border-radius: 0.5rem;
-  padding: 5px 15px;  
+  padding: 5px 15px;
   cursor: pointer;
 }
 .review-link:hover {
@@ -124,22 +139,20 @@ h3 {
   color: var(--primary200);
   background-color: var(--primary400);
   border-radius: 0.5rem;
-  padding: 5px 15px;  
+  padding: 5px 15px;
   cursor: pointer;
-  
 }
 .appointment:hover {
   background-color: var(--primary600);
 }
 
-.button-container{
+.button-container {
   display: flex;
   justify-content: center;
   align-items: center;
- 
 }
 .button {
   display: flex;
-    margin: 0 10px;
-  }
+  margin: 0 10px;
+}
 </style>
