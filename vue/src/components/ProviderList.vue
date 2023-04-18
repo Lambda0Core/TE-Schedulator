8a1bd65a987a81f23b9d793fe2d554e58036faf2
Bryce Huhtala <sketchius@gmail.com>
2023-04-18 09:20:57 -0400

    Styling changes to providerList

--- vue/src/components/ProviderList.vue
+++ vue/src/components/ProviderList.vue
@@ -10,7 +10,7 @@
         v-model.number="officeid"
         v-on:change="getProviders()"
       >
-        <option value="-1">All Offices</option>
+        <option value="-1" selected="selected">All Offices</option>
         <option v-for="office in offices" :key="office.id" :value="office.id">
           {{ office.name }}
         </option>
@@ -40,7 +40,7 @@ export default {
     return {
       providers: [],
       offices: [],
-      officeid: 0,
+      officeid: -1,
 
       user: {
         office: "",
@@ -83,6 +83,7 @@ export default {
       officeService.list().then((response) => {
         console.log(response.data);
         this.offices = response.data;
+        this.getProviders();
       });
     },
   },
@@ -107,7 +108,7 @@ h1 {
   margin: 3rem;
   margin-right: 15rem;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr;
   height: fit-content;
   grid-gap: 3rem;
