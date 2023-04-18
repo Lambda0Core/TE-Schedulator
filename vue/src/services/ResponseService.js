39d5b47e2790bba19b97374af079bfbf11a9a95a
Emily LeFebvre <Emily@Lefebvre.us>
2023-04-18 16:14:43 -0500

    basic structure for notifications

--- vue/src/services/ResponseService.js
+++ vue/src/services/ResponseService.js
@@ -5,14 +5,6 @@ export default {
   list() {
     return axios.get(`/response`)
   },
-<<<<<<< HEAD
-  getById(id) {
-    return axios.get(`/response/${id}`)
-  },
-  getByIdDetails() {
-    return axios.get(`/response`)
-  }
-=======
   create(response) {
    
     return axios.post('/response', response)
@@ -21,5 +13,4 @@ export default {
     return axios.get(`review/response/${id}`)
   },
 
->>>>>>> 8f10f2c975a4fb401dfedda14d5c08dce71359df
 }
